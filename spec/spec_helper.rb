ae266627a82aa194cf8aa012ad590be2278c67df
Rafaël Gonzalez <rafaelgonzalez@users.noreply.github.com>
2013-09-09 08:31:59 +0200

    Group files in simplecov coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_group "Extensions", "src/extensions"
+  add_group "Input", "src/input"
+  add_group "Models", "src/models"
+  add_group "Renderers", "src/renderers"
+end
 
 require 'initializer'
