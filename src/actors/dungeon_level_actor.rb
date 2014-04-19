487ae8f13e0bad57873e73747fedf8aa97e8423b
Rafaël Gonzalez <rafaelgonzalez@users.noreply.github.com>
2014-04-19 19:27:00 +0200

    Remove remaining methods calls to combat logger

--- src/actors/dungeon_level_actor.rb
+++ src/actors/dungeon_level_actor.rb
@@ -62,7 +62,6 @@ class DungeonLevelActor < Group
 
   def new_turn!
     @character_actors.map(&:reset_for_new_turn!)
-    log_message(I18n.t('actors.dungeon_level_actor.new_turn'))
   end
 
   # Public: Get the current character that responds to input.
